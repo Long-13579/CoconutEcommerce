f4cc407d7e859d7316bc31280b8c99632dde9afc
Lezt Waper <leztwaper@gmail.com>
2025-09-05 20:56:08 +0700

    Write tests and fix some bugs

--- apiApp/serializers/product_serializers.py
+++ apiApp/serializers/product_serializers.py
@@ -9,38 +9,8 @@ class ProductListSerializer(serializers.ModelSerializer):
         fields = ["id", "name", "slug", "image", "price"]
 
 class ProductDetailSerializer(serializers.ModelSerializer):
-    poor_review = serializers.SerializerMethodField()
-    fair_review = serializers.SerializerMethodField()
-    good_review = serializers.SerializerMethodField()
-    very_good_review = serializers.SerializerMethodField()
-    excellent_review = serializers.SerializerMethodField()
-    similar_products = serializers.SerializerMethodField()
-
     class Meta:
         model = Product
         fields = [
             "id", "name", "description", "slug", "image", "price",
-            "reviews", "rating", "similar_products",
-            "poor_review", "fair_review", "good_review",
-            "very_good_review", "excellent_review"
         ]
-
-    def get_similar_products(self, product):
-        products = Product.objects.filter(category=product.category).exclude(id=product.id)
-        from .product_serializers import ProductListSerializer
-        return ProductListSerializer(products, many=True).data
-
-    def get_poor_review(self, product):
-        return product.reviews.filter(rating=1).count()
-
-    def get_fair_review(self, product):
-        return product.reviews.filter(rating=2).count()
-
-    def get_good_review(self, product):
-        return product.reviews.filter(rating=3).count()
-
-    def get_very_good_review(self, product):
-        return product.reviews.filter(rating=4).count()
-
-    def get_excellent_review(self, product):
-        return product.reviews.filter(rating=5).count()
--- apiApp/urls/products.py
+++ apiApp/urls/products.py
@@ -3,6 +3,6 @@ from ..views import product_views as views
 
 urlpatterns = [
     path("list", views.product_list, name="product_list"),
-    path("<slug:slug>", views.product_detail, name="product_detail"),
     path("search", views.product_search, name="product_search"),
+    path("<slug:slug>", views.product_detail, name="product_detail"),
 ]
