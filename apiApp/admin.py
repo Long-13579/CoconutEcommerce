54d867013895fca1ae7b47acc096c632ae5b2320
DangThiMaiThuyen2003 <dangmaithuyen3002@gmail.com>
2025-09-23 00:35:04 +0700

    created admin dashboard UI

--- apiApp/admin.py
+++ apiApp/admin.py
@@ -1,3 +1,14 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models.address import CustomerAddress
+from .models.cart import Cart, CartItem
+from .models.category import Category
+from .models.product import Product
+from .models.user import CustomUser
+
+admin.site.register(CustomerAddress)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(CustomUser)
--- ecommerceApiProject/settings.py
+++ ecommerceApiProject/settings.py
@@ -81,14 +81,15 @@ WSGI_APPLICATION = 'ecommerceApiProject.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# Sử dụng SQLite cho phát triển
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
