5e4e2863a705b9875086bccdf94edb4eb389457c
Lezt Waper <leztwaper@gmail.com>
2025-09-25 20:58:35 +0700

    Refactoring: Change Payment Method
    Change payment method from stripe to momo
    
    Add the setting variable for the momo payment
    
    Update the git ignore and delete the unnecessary file and folder

--- apiApp/urls/orders.py
+++ apiApp/urls/orders.py
@@ -3,6 +3,6 @@ from ..views import order_views as views
 
 urlpatterns = [
     path("create_checkout_session/", views.create_checkout_session, name="create_checkout_session"),
-    path("webhook/", views.my_webhook_view, name="webhook"),
+    path("finish_checkout", views.finish_checkout, name="finish checkout"),
     path("get_orders", views.get_orders, name="get_orders"),
 ]
